3aa7de98cfb884df2a801f35ddff03fa48d0f851
Andy <andy.hsu.17@gmail.com>
2020-05-21 00:46:01 -0600

    initial commit

---
+++ GNU ARM v7.2.1 - Debug/Graphics/dmd_display.d
@@ -0,0 +1,35 @@
+Graphics/dmd_display.o: \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v2.6/platform/middleware/glib/dmd/display/dmd_display.c \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v2.6//hardware/kit/common/drivers/display.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v2.6//hardware/kit/common/drivers/emstatus.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v2.6//platform/middleware/glib/em_types.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v2.6//hardware/kit/common/drivers/displayconfigall.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v2.6//hardware/kit/SLSTK3402A_EFM32PG12/config/displaypalconfig.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v2.6//hardware/kit/SLSTK3402A_EFM32PG12/config/displayconfig.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v2.6//app/mcu_example/SLSTK3402A_EFM32PG12/humitemp/displayconfigapp.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v2.6//hardware/kit/SLSTK3402A_EFM32PG12/config/displayls013b7dh03config.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v2.6//hardware/kit/common/drivers/displayls013b7dh03.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v2.6//hardware/kit/SLSTK3402A_EFM32PG12/config/displayls013b7dh03config.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v2.6//platform/middleware/glib/dmd/dmd.h
+
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v2.6//hardware/kit/common/drivers/display.h:
+
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v2.6//hardware/kit/common/drivers/emstatus.h:
+
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v2.6//platform/middleware/glib/em_types.h:
+
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v2.6//hardware/kit/common/drivers/displayconfigall.h:
+
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v2.6//hardware/kit/SLSTK3402A_EFM32PG12/config/displaypalconfig.h:
+
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v2.6//hardware/kit/SLSTK3402A_EFM32PG12/config/displayconfig.h:
+
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v2.6//app/mcu_example/SLSTK3402A_EFM32PG12/humitemp/displayconfigapp.h:
+
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v2.6//hardware/kit/SLSTK3402A_EFM32PG12/config/displayls013b7dh03config.h:
+
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v2.6//hardware/kit/common/drivers/displayls013b7dh03.h:
+
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v2.6//hardware/kit/SLSTK3402A_EFM32PG12/config/displayls013b7dh03config.h:
+
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v2.6//platform/middleware/glib/dmd/dmd.h:
---
+++ src/humitemp.c
@@ -0,0 +1,339 @@
+/***************************************************************************//**
+ * @file
+ * @brief Relative humidity and temperature sensor demo for SLSTK3402A
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2018 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * The licensor of this software is Silicon Laboratories Inc. Your use of this
+ * software is governed by the terms of Silicon Labs Master Software License
+ * Agreement (MSLA) available at
+ * www.silabs.com/about-us/legal/master-software-license-agreement. This
+ * software is distributed to you in Source Code format and is governed by the
+ * sections of the MSLA applicable to Source Code.
+ *
+ ******************************************************************************/
+
+#include "em_device.h"
+#include "em_chip.h"
+#include "em_cmu.h"
+#include "em_emu.h"
+#include "em_gpio.h"
+#include "cpt112s_config.h"
+#include "i2cspm.h"
+#include "si7013.h"
+#include "glib.h"
+#include "rtcdriver.h"
+#include "graphics.h"
+#include "em_adc.h"
+#include "bspconfig.h"
+#include "udelay.h"
+/***************************************************************************//**
+ * Local defines
+ ******************************************************************************/
+
+/** Time (in ms) between periodic updates of the measurements. */
+#define PERIODIC_UPDATE_MS      2000
+#define IR_SENSOR_ADDRESS       0x00
+#define IR_MEASURE_COMMAND      0xAF
+#define IR_READ_SENSOR_OR  0x01  // logical "or" into slave address to specify read or write
+#define IR_WRITE_SENSOR_OR 0x00
+
+/*Calculation Defines*/
+#define TC 		-.0046
+#define Tref 	26.93
+#define k4comp .0005161 
+#define k3comp .5639
+#define k2comp 231.1
+#define k1comp 42070
+#define k0comp -1312000
+
+#define k4obj -1.029E-26
+#define k3obj 1.787E-19
+#define k2obj -1.631E-12
+#define k1obj 1.802E-5
+#define k0obj 26.93
+
+/*
+Global Variables
+*/
+//static GLIB_Context_t glibContext;          /* Global glib context */
+
+/***************************************************************************//**
+ * Local variables
+ ******************************************************************************/
+/* RTC callback parameters. */
+static void (*rtcCallback)(void*) = 0;
+static void * rtcCallbackArg = 0;
+
+/** This flag tracks if we need to update the display
+ *  (animations or measurements). */
+static volatile bool updateDisplay = true;
+/** This flag tracks if we need to perform a new
+ *  measurement. */
+static volatile bool updateMeasurement = true;
+
+/** Timer used for periodic update of the measurements. */
+RTCDRV_TimerID_t periodicUpdateTimerId;
+
+/***************************************************************************//**
+ * Local prototypes
+ ******************************************************************************/
+static void gpioSetup(void);
+static void periodicUpdateCallback(RTCDRV_TimerID_t id, void *user);
+static void memLcdCallback(RTCDRV_TimerID_t id, void *user);
+
+/***************************************************************************//**
+ * @brief  Helper function to perform data measurements.
+ ******************************************************************************/
+//static int performMeasurements(I2C_TypeDef *i2c, uint32_t *rhData, int32_t *tData)
+//{
+//  Si7013_MeasureRHAndTemp(i2c, SI7021_ADDR, rhData, tData);
+//  return 0;
+//}
+
+
+static int32_t i2c_write_sensor(I2C_TypeDef *i2c, uint8_t addr, int64_t *data,
+                              uint8_t command)
+{
+  I2C_TransferSeq_TypeDef    seq;
+  I2C_TransferReturn_TypeDef ret;
+  uint8_t                    i2c_read_data[2];
+  uint8_t                    i2c_write_data[1];
+
+  seq.addr  = addr;
+  seq.flags = I2C_FLAG_WRITE_READ;
+  /* Select command to issue */
+  i2c_write_data[0] = command;
+  seq.buf[0].data   = i2c_write_data;
+  seq.buf[0].len    = 1;
+  /* Select location/length of data to be read */
+  seq.buf[1].data = i2c_read_data;
+  seq.buf[1].len  = 1;
+
+  ret = I2CSPM_Transfer(i2c, &seq);
+
+  if (ret != i2cTransferDone) {
+    *data = 0;
+    return((int) ret);
+  }
+
+  *data = ((uint32_t) i2c_read_data[0] << 8) + (i2c_read_data[1] & 0xfc);
+
+  return((int32_t) 0);
+}
+
+static int32_t i2c_read_sensor(I2C_TypeDef *i2c, uint8_t addr, int64_t *data,
+                              uint8_t command)
+{
+  I2C_TransferSeq_TypeDef    seq;
+  I2C_TransferReturn_TypeDef ret;
+  uint8_t                    i2c_read_data[7];
+  uint8_t                    i2c_write_data[1];
+
+  seq.addr  = addr;
+  seq.flags = I2C_FLAG_WRITE_READ;
+  /* Select command to issue */
+  i2c_write_data[0] = command;
+  seq.buf[0].data   = i2c_write_data;
+  seq.buf[0].len    = 0;
+  /* Select location/length of data to be read */
+  seq.buf[1].data = i2c_read_data;
+  seq.buf[1].len  = 7;
+
+  ret = I2CSPM_Transfer(i2c, &seq);
+
+  if (ret != i2cTransferDone) {
+    *data = 0;
+    return((int) ret);
+  }
+
+  *data = (int64_t) (((int64_t)i2c_read_data[0] << 48) + ((int64_t)i2c_read_data[1] << 40) + ((int64_t)i2c_read_data[2] << 32) + ((int64_t)i2c_read_data[3] << 24) +
+                    ((int64_t)i2c_read_data[4] << 16) + ((int64_t)i2c_read_data[5] << 8) + ((int64_t)i2c_read_data[6]));
+
+  return((int32_t) 2);
+}
+
+
+
+/***************************************************************************//**
+ * @brief Setup GPIO, enable sensor isolation switch
+ ******************************************************************************/
+static void gpioSetup(void)
+{
+  /* Enable GPIO clock */
+  CMU_ClockEnable(cmuClock_GPIO, true);
+
+  /* Enable Si7021 sensor isolation switch */
+  GPIO_PinModeSet(CS0_SENSOR_EN_PORT, CS0_SENSOR_EN_PIN, gpioModePushPull, 1);
+}
+
+/***************************************************************************//**
+ * @brief   The actual callback for Memory LCD toggling
+ * @param[in] id
+ *   The id of the RTC timer (not used)
+ ******************************************************************************/
+static void memLcdCallback(RTCDRV_TimerID_t id, void *user)
+{
+  (void) id;
+  (void) user;
+  rtcCallback(rtcCallbackArg);
+}
+
+/***************************************************************************//**
+ * @brief   Register a callback function at the given frequency.
+ *
+ * @param[in] pFunction  Pointer to function that should be called at the
+ *                       given frequency.
+ * @param[in] argument   Argument to be given to the function.
+ * @param[in] frequency  Frequency at which to call function at.
+ *
+ * @return  0 for successful or
+ *         -1 if the requested frequency does not match the RTCC frequency.
+ ******************************************************************************/
+int rtccIntCallbackRegister(void (*pFunction)(void*),
+                            void* argument,
+                            unsigned int frequency)
+{
+  RTCDRV_TimerID_t timerId;
+  rtcCallback    = pFunction;
+  rtcCallbackArg = argument;
+
+  RTCDRV_AllocateTimer(&timerId);
+
+  RTCDRV_StartTimer(timerId, rtcdrvTimerTypePeriodic, 1000 / frequency,
+                    memLcdCallback, 0);
+
+  return 0;
+}
+
+/***************************************************************************//**
+ * @brief Callback used to count between measurement updates
+ ******************************************************************************/
+static void periodicUpdateCallback(RTCDRV_TimerID_t id, void *user)
+{
+  (void) id;
+  (void) user;
+  updateDisplay = true;
+  updateMeasurement = true;
+}
+
+
+/***************************************************************************//**
+ * @brief  Main function
+ ******************************************************************************/
+int main(void)
+{
+  // I2CSPM_Init_TypeDef i2cInit = I2CSPM_INIT_DEFAULT;
+  I2CSPM_Init_TypeDef i2cInit;
+  i2cInit.port = I2C0;
+  i2cInit.sclPort = gpioPortC;
+  i2cInit.sclPin = 11;
+  i2cInit.sdaPort = gpioPortC;
+  i2cInit.sdaPin = 10;
+  i2cInit.portLocationScl = 15;
+  i2cInit.portLocationSda = 15;
+  i2cInit.i2cRefFreq = 0;
+  i2cInit.i2cMaxFreq = I2C_FREQ_STANDARD_MAX;
+  i2cInit.i2cClhr = i2cClockHLRStandard;
+
+  EMU_DCDCInit_TypeDef dcdcInit = EMU_DCDCINIT_STK_DEFAULT;
+  CMU_HFXOInit_TypeDef hfxoInit = CMU_HFXOINIT_DEFAULT;
+  bool             si7013_status;
+  int64_t          tempData = 0;
+
+// IR Temp local variables
+  uint64_t      ADCsen = 0;
+  uint64_t      ADCobj = 0;
+  float      ADCcomp = 0;
+  float      ADCcompTC = 0;
+  float      Tsen = 0;
+  float      Fsen = 0;
+  uint8_t statusByte = 0;
+  float temp = 0;
+  float TCF;
+  float offset;
+  float offsetTC;
+  float Tobj;
+  float Fobj;
+
+
+  /* Chip errata */
+  CHIP_Init();
+
+  /* Initalize hardware */
+  EMU_DCDCInit(&dcdcInit);
+  CMU_HFXOInit(&hfxoInit);
+
+  /* Switch HFCLK to HFXO and disable HFRCO */
+  CMU_ClockSelectSet(cmuClock_HF, cmuSelect_HFXO);
+  CMU_OscillatorEnable(cmuOsc_HFRCO, false, false);
+
+  /* Initalize other peripherals and drivers */
+  gpioSetup();
+  RTCDRV_Init();
+  GRAPHICS_Init();
+  I2CSPM_Init(&i2cInit);
+
+  /* Get initial sensor status */
+  // si7013_status = Si7013_Detect(i2cInit.port, SI7021_ADDR, 0);
+
+  /* Set up periodic update of the display. */
+  RTCDRV_AllocateTimer(&periodicUpdateTimerId);
+  RTCDRV_StartTimer(periodicUpdateTimerId, rtcdrvTimerTypePeriodic,
+                    PERIODIC_UPDATE_MS, periodicUpdateCallback, 0);
+
+//  GRAPHICS_ShowStatus(si7013_status);
+  EMU_EnterEM2(false);
+
+  updateDisplay = true;
+
+  while (true)
+  {
+    // if (updateMeasurement) 
+    // {
+    //   // performMeasurements(i2cInit.port, &rhData, &tempData);
+    //   i2c_write_sensor(i2cInit.port, IR_SENSOR_ADDRESS, &tempData, IR_READ_SENSOR_COMMAND);
+    //   updateMeasurement = false;
+    // }
+
+    // if (updateDisplay) 
+    // {
+    //   updateDisplay = false;
+    //   GRAPHICS_Draw(tempData, rhData);
+    // }
+    // EMU_EnterEM2(false);
+    i2c_write_sensor(i2cInit.port, IR_SENSOR_ADDRESS | IR_WRITE_SENSOR_OR, &tempData, IR_MEASURE_COMMAND);
+    UDELAY_Delay(100000);
+    i2c_read_sensor(i2cInit.port, IR_SENSOR_ADDRESS | IR_READ_SENSOR_OR, &tempData, IR_MEASURE_COMMAND);
+    statusByte = (uint8_t)(tempData >> 48);
+    if(statusByte != 0x60)  // check if we are busy or memory error
+    {
+      ADCsen = (tempData & 0xFFFFFF);
+      temp = (float)ADCsen / (2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2);
+      Tsen = (temp * 105) - 20;
+      Fsen = (1.8 * Tsen) + 32;
+
+      TCF = 1 + ((Tsen - Tref) * TC);
+      offset = (k4comp * (Tsen * Tsen * Tsen * Tsen)) +
+                (k3comp * (Tsen * Tsen * Tsen)) + 
+                (k2comp * (Tsen * Tsen)) + 
+                (k1comp * Tsen) + 
+                (k0comp); 
+      offsetTC = offset * TCF;
+      ADCobj = tempData >> 24;
+      ADCobj &= 0xFFFFFF;
+      ADCcomp = offsetTC - (2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2) + (float)ADCobj;
+      ADCcompTC = ADCcomp / TCF;
+      Tobj = (k4obj * ADCcompTC * ADCcompTC * ADCcompTC * ADCcompTC) +
+              (k3obj * ADCcompTC * ADCcompTC * ADCcompTC) + 
+              (k2obj * ADCcompTC * ADCcompTC) + 
+              (k1obj * ADCcompTC) + 
+              (k0obj);
+      Fobj = 1.8 * Tobj + 32;
+    }
+    EMU_EnterEM2(false);
+
+  }
+}
